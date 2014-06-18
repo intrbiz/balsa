65621264ed34dff8211d1c4e07f29b9c1019bf4d
Chris Ellis <chris@intrbiz.com>
2014-06-19 00:54:43 +0100

    Add a container element
    The <container> element can be used to apply a rendered rule to a set of
    components.  The <container> element does not output any HTML.

---
+++ balsa-core/src/main/java/com/intrbiz/balsa/view/core/template/ContainerComponent.java
@@ -0,0 +1,11 @@
+package com.intrbiz.balsa.view.core.template;
+
+import com.intrbiz.balsa.view.component.Component;
+
+public class ContainerComponent extends Component
+{
+    public ContainerComponent()
+    {
+        super();
+    }
+}
---
+++ balsa-core/src/main/java/com/intrbiz/balsa/view/core/template/ContainerRenderer.java
@@ -0,0 +1,7 @@
+package com.intrbiz.balsa.view.core.template;
+
+import com.intrbiz.balsa.view.renderer.Renderer;
+
+public class ContainerRenderer extends Renderer
+{
+}
