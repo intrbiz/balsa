d60bf80996d82c782915d9420b5cade802213073
Chris Ellis <chris@intrbiz.com>
2014-08-13 22:19:05 +0100

    Additional metadata parsing helpers

--- balsa-core/src/main/java/com/intrbiz/balsa/engine/impl/view/ViewMetadataParser.java
+++ balsa-core/src/main/java/com/intrbiz/balsa/engine/impl/view/ViewMetadataParser.java
@@ -1,7 +1,11 @@
 package com.intrbiz.balsa.engine.impl.view;
 
+import java.io.File;
+import java.io.FileReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.apache.log4j.Logger;
 
 import com.intrbiz.balsa.engine.view.ViewMetadata;
 
@@ -19,10 +23,36 @@ import com.intrbiz.balsa.engine.view.ViewMetadata;
  */
 public class ViewMetadataParser
 {
+    private static Logger logger = Logger.getLogger(ViewMetadataParser.class);
 
     private static final Pattern META_BLOCK = Pattern.compile("(?m)^---$((?s).+)^---$");
 
     private static final Pattern META_DATA = Pattern.compile("(?m)^([A-Za-z0-9_]+):\\s*(.*)$");
+    
+    public static ViewMetadata extractMetadata(File file)
+    {
+        try (FileReader in = new FileReader(file))
+        {
+            char[] buffer = new char[1024];
+            int length = in.read(buffer);
+            if (length > 0)
+            {
+                return extractMetadata(new String(buffer, 0, length));
+            }
+        }
+        catch (Exception e)
+        {
+            logger.warn("Failed to parse view metadata", e);
+        }
+        return null;
+    }
+    
+    public static ViewMetadata extractMetadata(String text)
+    {
+        ViewMetadata md = new ViewMetadata();
+        extractMetadata(text, md);
+        return md;
+    }
 
     public static String extractMetadata(String text, ViewMetadata metadata)
     {
