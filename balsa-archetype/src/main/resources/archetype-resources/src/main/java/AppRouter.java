8a88f643c7d84473bd9c567aad37162291e859b3
Chris Ellis <chris@intrbiz.com>
2014-08-15 00:55:10 +0100

    Add error handlers to archetype

--- balsa-archetype/src/main/resources/archetype-resources/src/main/java/AppRouter.java
+++ balsa-archetype/src/main/resources/archetype-resources/src/main/java/AppRouter.java
@@ -4,8 +4,14 @@
 package ${package};
 
 import com.intrbiz.balsa.engine.route.Router;
+import com.intrbiz.balsa.error.http.BalsaNotFound;
+import com.intrbiz.balsa.error.view.BalsaViewNotFound;
+import com.intrbiz.balsa.http.HTTP.HTTPStatus;
 import com.intrbiz.metadata.Any;
+import com.intrbiz.metadata.Catch;
+import com.intrbiz.metadata.Order;
 import com.intrbiz.metadata.Prefix;
+import com.intrbiz.metadata.Status;
 import com.intrbiz.metadata.Template;
 
 // Routes for the URL root
@@ -18,5 +24,28 @@ public class AppRouter extends Router<App>
     {
         // Encode the index view
         encode("index");
+    }
+       
+    @Any("/**")
+    @Catch(BalsaNotFound.class)
+    @Status(HTTPStatus.NotFound)
+    public void notFound()
+    {
+        // Encode the 404 view
+        encode("notfound");
+    }
+    
+    @Any("/**")
+    @Catch()
+    @Order(Order.LAST)
+    @Status(HTTPStatus.InternalServerError)
+    public void error()
+    {
+        // Log the internal error
+        System.err.println("Internal server error whilst processing request");
+        if (balsa().getException() != null)
+            balsa().getException().printStackTrace();
+        // Encode the error view
+        encode("error");
     }
 }
