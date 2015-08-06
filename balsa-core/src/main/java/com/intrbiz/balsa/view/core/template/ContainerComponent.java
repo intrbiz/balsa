16b518f20a34856049fc30c920379ea6df2408e4
Chris Ellis <chris@intrbiz.com>
2015-08-06 22:04:19 +0100

    The container component can set local entities
    The container component can now set a local entity, this allows
    an entity to be computed once and used by the tree of child
    components.  This follows the same logic as the include component.
    
    <container data-myentity="#{'some expression'}">
      <span>#{myentity}</span>
    </container>

--- balsa-core/src/main/java/com/intrbiz/balsa/view/core/template/ContainerComponent.java
+++ balsa-core/src/main/java/com/intrbiz/balsa/view/core/template/ContainerComponent.java
@@ -1,11 +1,33 @@
 package com.intrbiz.balsa.view.core.template;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import com.intrbiz.balsa.view.component.Component;
+import com.intrbiz.express.value.ValueExpression;
 
 public class ContainerComponent extends Component
 {
     public ContainerComponent()
     {
         super();
+    }
+    
+    /**
+     * Get the map of variables which should be bound before encoding the this container
+     */
+    public Map<String, ValueExpression> getDataBindings()
+    {
+        Map<String, ValueExpression> bindings = new HashMap<String, ValueExpression>();
+        for (Entry<String, ValueExpression> attribute : this.getAttributes().entrySet())
+        {
+            if (attribute.getKey().startsWith("data-"))
+            {
+                // strip of the 'data-' prefix
+                bindings.put(attribute.getKey().substring(5), attribute.getValue());
+            }
+        }
+        return bindings;
     }
 }
--- balsa-core/src/main/java/com/intrbiz/balsa/view/core/template/ContainerRenderer.java
+++ balsa-core/src/main/java/com/intrbiz/balsa/view/core/template/ContainerRenderer.java
@@ -1,7 +1,45 @@
 package com.intrbiz.balsa.view.core.template;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.intrbiz.balsa.BalsaContext;
+import com.intrbiz.balsa.BalsaException;
+import com.intrbiz.balsa.util.BalsaWriter;
+import com.intrbiz.balsa.view.component.Component;
 import com.intrbiz.balsa.view.renderer.Renderer;
+import com.intrbiz.express.ExpressContext;
+import com.intrbiz.express.ExpressException;
+import com.intrbiz.express.value.ValueExpression;
 
 public class ContainerRenderer extends Renderer
 {
+    @Override
+    public void encodeChildren(Component component, BalsaContext context, BalsaWriter to) throws IOException, BalsaException, ExpressException
+    {
+        ContainerComponent inc = (ContainerComponent) component;
+        Map<String, ValueExpression> bindings = inc.getDataBindings();
+        // enter an express frame to isolate variables
+        ExpressContext elctx = context.getExpressContext();
+        elctx.enterFrame(false);
+        try
+        {
+            // set variables for each data binding
+            for (Entry<String, ValueExpression> binding : bindings.entrySet())
+            {
+                // ensure that we pre-eval expressions, as the point is to locally cache a value
+                elctx.setEntity(binding.getKey(), binding.getValue().get(elctx, component), inc);
+            }
+            // encode our children
+            for (Component child : component.getChildren())
+            {
+                child.encode(context, to);
+            }
+        }
+        finally
+        {
+            elctx.exitFrame();
+        }
+    }
 }
