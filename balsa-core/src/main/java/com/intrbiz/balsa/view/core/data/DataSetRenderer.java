fb0929eefbcb22bef1cab97239099db454482a38
Chris Ellis <chris@intrbiz.com>
2015-09-14 08:43:36 +0100

    Handle more types of collections

--- balsa-core/src/main/java/com/intrbiz/balsa/view/core/data/DataSetRenderer.java
+++ balsa-core/src/main/java/com/intrbiz/balsa/view/core/data/DataSetRenderer.java
@@ -2,6 +2,8 @@ package com.intrbiz.balsa.view.core.data;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 import com.intrbiz.balsa.BalsaContext;
 import com.intrbiz.balsa.BalsaException;
@@ -13,6 +15,85 @@ import com.intrbiz.express.ExpressException;
 
 public class DataSetRenderer extends Renderer
 {
+    private Collection<?> toCollection(Object value)
+    {
+        // no need to convert
+        if (value instanceof Collection) return (Collection<?>) value;
+        // convert to a list, otherwise empty
+        List<Object> values = new LinkedList<Object>();
+        if (value instanceof Iterable)
+        {
+            for (Object val : (Iterable<?>) values)
+            {
+                values.add(val);
+            }
+        }
+        else if (value instanceof Object[])
+        {
+            for (Object val : (Object[]) value)
+            {
+                values.add(val);
+            }
+        }
+        else if (value instanceof double[])
+        {
+            for (double val : (double[]) value)
+            {
+                values.add(val);
+            }
+        }
+        else if (value instanceof float[])
+        {
+            for (float val : (float[]) value)
+            {
+                values.add(val);
+            }
+        }
+        else if (value instanceof long[])
+        {
+            for (long val : (long[]) value)
+            {
+                values.add(val);
+            }
+        }
+        else if (value instanceof int[])
+        {
+            for (int val : (int[]) value)
+            {
+                values.add(val);
+            }
+        }
+        else if (value instanceof short[])
+        {
+            for (short val : (short[]) value)
+            {
+                values.add(val);
+            }
+        }
+        else if (value instanceof byte[])
+        {
+            for (byte val : (byte[]) value)
+            {
+                values.add(val);
+            }
+        }
+        else if (value instanceof char[])
+        {
+            for (char val : (char[]) value)
+            {
+                values.add(val);
+            }
+        }
+        else if (value instanceof boolean[])
+        {
+            for (boolean val : (boolean[]) value)
+            {
+                values.add(val);
+            }
+        }
+        return values;
+    }
+    
     @Override
     public void encodeChildren(Component component, BalsaContext context, BalsaWriter to) throws IOException, BalsaException, ExpressException
     {
@@ -21,8 +102,7 @@ public class DataSetRenderer extends Renderer
         Object value = dataSet.evaluateValue();
         if (value != null)
         {
-            if (!(value instanceof Collection)) throw new BalsaException("Cannot encode DataSet, the value must be a collection.");
-            Collection<?> set = (Collection<?>) value;
+            Collection<?> set = toCollection(value);
             //
             String varName = dataSet.evaluateVar();
             String rowVarName = varName + "_rownum";
